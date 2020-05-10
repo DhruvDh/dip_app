afb046d1f2f1c4473fc3a5964a498ea1c00aebbe
Shreeya Gupta <shreeyag11@gmail.com>
2020-05-10 10:44:39 -0400

    added notification bar

---
+++ src/components/FileNotification.vue
@@ -0,0 +1,19 @@
+<template>
+        <div class="notification is-primary notify">
+            Successfully parsed 
+        </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .notify {
+        padding: 0.8rem 0rem 0.8rem 0rem !important;
+        margin-bottom: 0px !important;
+        text-align: center;
+    }
+</style>
