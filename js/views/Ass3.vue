baacf39313daa942acc6970c83476f792ff24ffa
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2020-06-08 17:06:01 -0400

    check on other computer

--- js/views/Ass3.vue
+++ js/views/Ass3.vue
@@ -1,8 +1,17 @@
 <template>
-  <section class="section is-full" @drop="dropHandler" @dragover="dragOverHandler">
-    <div class="column is-narrow is-size-2 has-text-centered">Assignment 3</div>
+  <section
+    class="section is-full"
+    @drop="dropHandler"
+    @dragover="dragOverHandler"
+  >
+    <div class="column is-narrow is-size-2 has-text-centered">
+      Assignment 3
+    </div>
 
-    <ImageViewer :file-obj="$store.state.ass3" :mount-div="mountDiv" />
+    <ImageViewer
+      :file-obj="$store.state.ass3"
+      :mount-div="mountDiv"
+    />
     <Log
       :p-errors="$store.state.ass3.fileParseErrors"
       :p-success="$store.state.ass3.fileParseSuccessful"
@@ -16,7 +25,11 @@
       <KernelFormat />
 
       <b-field label="Enter Kernel">
-        <b-input v-model="kernelText" @input="kernelTextChange"></b-input>
+        <b-input
+          v-model="kernelText"
+          custom-class="kernelInput"
+          @input="kernelTextChange"
+        />
       </b-field>
 
       <Log
@@ -28,39 +41,38 @@
       <div class="kernelHead">
         <div class="kernel">
           <b-field>
-            <b-input v-model="kernelArray[0][0]"></b-input>
+            <b-input v-model="kernelArray00" />
           </b-field>
           <b-field>
-            <b-input v-model="kernelArray[0][1]"></b-input>
+            <b-input v-model="kernelArray01" />
           </b-field>
           <b-field>
-            <b-input v-model="kernelArray[0][2]"></b-input>
-          </b-field>
-        </div>
-        <div class="kernel">
-        <b-field>
-            <b-input v-model="kernelArray[1][0]"></b-input>
-          </b-field>
-          <b-field>
-            <b-input v-model="kernelArray[1][1]"></b-input>
-          </b-field>
-          <b-field>
-            <b-input v-model="kernelArray[1][2]"></b-input>
+            <b-input v-model="kernelArray02" />
           </b-field>
         </div>
         <div class="kernel">
           <b-field>
-            <b-input v-model="kernelArray[2][0]"></b-input>
+            <b-input v-model="kernelArray10" />
           </b-field>
           <b-field>
-            <b-input v-model="kernelArray[2][1]"></b-input>
+            <b-input v-model="kernelArray11" />
           </b-field>
           <b-field>
-            <b-input v-model="kernelArray[2][2]"></b-input>
+            <b-input v-model="kernelArray12" />
+          </b-field>
+        </div>
+        <div class="kernel">
+          <b-field>
+            <b-input v-model="kernelArray20" />
+          </b-field>
+          <b-field>
+            <b-input v-model="kernelArray21" />
+          </b-field>
+          <b-field>
+            <b-input v-model="kernelArray22" />
           </b-field>
         </div>
       </div>
-      
     </div>
     <FilePicker :page-name="pageName" />
     <MdPage />
@@ -68,11 +80,11 @@
 </template>
 
 <script>
-import ImageViewer from "../components/ImageViewer.vue";
-import FilePicker from "../components/FilePicker.vue";
-import Log from "../components/Log.vue";
-import KernelFormat from "../md/KernelFormat.vue";
-import MdPage from "../md/Ass3.vue";
+import ImageViewer from '../components/ImageViewer.vue';
+import FilePicker from '../components/FilePicker.vue';
+import Log from '../components/Log.vue';
+import KernelFormat from '../md/KernelFormat.vue';
+import MdPage from '../md/Ass3.vue';
 
 export default {
   components: {
@@ -80,21 +92,24 @@ export default {
     FilePicker,
     Log,
     MdPage,
-    KernelFormat
+    KernelFormat,
   },
   data() {
     return {
-      mountDiv: "ass3Div",
+      mountDiv: 'ass3Div',
       kernelParseSuccessful: true,
-      kernelParseErrors: "",
-      kernelText: "[[1.2, 1.2, 1.2], [1.2, 1.2, 1.2], [1.2, 1.2, 1.2]]",
-      kernelArray: [[1.2, 1.2, 1.2], [1.2, 1.2, 1.2], [1.2, 1.2, 1.2]],
+      kernelParseErrors: '',
+      kernelText: '[ [1.2, 1.2, 1.2] , [1.2, 1.2, 1.2] , [1.2, 1.2, 1.2] ]',
+      kernelArray00: 1.2,
+      kernelArray01: 1.2,
+      kernelArray02: 1.2,
+      kernelArray10: 1.2,
+      kernelArray11: 1.2,
+      kernelArray12: 1.2,
+      kernelArray20: 1.2,
+      kernelArray21: 1.2,
+      kernelArray22: 1.2,
     };
-  },
-  watch: {
-      kernelText(newVal) {
-          console.log(newVal);
-      }
   },
   computed: {
     pageName() {
@@ -104,26 +119,31 @@ export default {
       if (this.$store.state.ass3.file) {
         return this.$store.state.ass3.file.name;
       }
-      return "";
+      return '';
     },
     fileHeight() {
       if (this.$store.state.ass3.file) {
         return this.$store.state.ass3.file.height;
       }
-      return "";
+      return '';
     },
     fileWidth() {
       if (this.$store.state.ass3.file) {
         return this.$store.state.ass3.file.width;
       }
-      return "";
+      return '';
     },
     fileType() {
       if (this.$store.state.ass3.file) {
         return this.$store.state.ass3.file.file_type;
       }
-      return "";
-    }
+      return '';
+    },
+  },
+  watch: {
+    kernelText(newVal) {
+      console.log(newVal);
+    },
   },
   methods: {
     dropHandler(ev) {
@@ -134,11 +154,11 @@ export default {
         // Use DataTransferItemList interface to access the file(s)
         for (let i = 0; i < ev.dataTransfer.items.length; i += 1) {
           // If dropped items aren't files, reject them
-          if (ev.dataTransfer.items[i].kind === "file") {
+          if (ev.dataTransfer.items[i].kind === 'file') {
             const file = ev.dataTransfer.items[i].getAsFile();
-            this.$store.dispatch("PARSE_FILE", {
+            this.$store.dispatch('PARSE_FILE', {
               file,
-              type: this.pageName
+              type: this.pageName,
             });
           }
         }
@@ -146,7 +166,7 @@ export default {
         // Use DataTransfer interface to access the file(s)
         for (let i = 0; i < ev.dataTransfer.files.length; i += 1) {
           console.log(
-            `2. ... file[${i}].name = ${ev.dataTransfer.files[i].name}`
+            `2. ... file[${i}].name = ${ev.dataTransfer.files[i].name}`,
           );
         }
       }
@@ -162,12 +182,17 @@ export default {
         this.kernelParseSuccessful = false;
         this.kernelParseErrors = err;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style>
+@font-face {
+  font-family: "LM Roman 10";
+  src: url("/font/lmroman10-regular.otf");
+}
+
 .kernel {
   width: 5%;
 }
@@ -176,4 +201,9 @@ export default {
   display: flex;
   margin: 2rem;
 }
+
+.kernelInput {
+  font-family: "LM Roman 10" !important;
+}
+
 </style>
